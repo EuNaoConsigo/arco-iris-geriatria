4a49d5df09f8a0d82e4da01041bbc1b6000eee7f
Esdras Oliveira <esdras_obrainer@hotmail.com>
2024-11-17 14:21:20 -0300

    mais mudanças no Build

--- build.sh
+++ build.sh
@@ -8,13 +8,10 @@ python -m venv venv
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Convert static asset files
-python manage.py collectstatic --no-input
-
-
 python manage.py makemigrations
 
 # Apply any outstanding database migrations
 python manage.py migrate
 
-python manage.py migrate --run-syncdb
+# Convert static asset files
+python manage.py collectstatic --no-input
