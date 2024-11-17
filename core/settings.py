1a4e21d571efdddd2f2e31c881e4d8804d2cee9c
Esdras Oliveira <esdras_obrainer@hotmail.com>
2024-11-17 12:51:50 -0300

    Teste(Database)

--- core/settings.py
+++ core/settings.py
@@ -111,8 +111,12 @@ else:
 
     DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_database_9fy7',
+        'USER': 'my_database_9fy7_user',
+        'PASSWORD': 'your_password',
+        'HOST': 'your_host',
+        'PORT': '5432',
     }
     }
 
