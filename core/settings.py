3264d6900fd12741fbe6d12fc3d9fffdc81bac0a
Esdras Oliveira <esdras_obrainer@hotmail.com>
2024-11-17 13:23:58 -0300

    Teste Database 2

--- core/settings.py
+++ core/settings.py
@@ -110,10 +110,9 @@ if not DEBUG:
 else:
 
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')  # Isso pega a URL do banco de dados da variável de ambiente
+    )
     }
 
 
