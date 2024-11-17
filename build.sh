485c021ac4d214afb12a59e5cc6597991d2413ee
Esdras Oliveira <esdras_obrainer@hotmail.com>
2024-11-17 13:58:23 -0300

    Mudanças na Build

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+# Criar e ativar o ambiente virtual, se necessário
+python -m venv venv
+
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
